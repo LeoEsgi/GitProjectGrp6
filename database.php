dcaf1fb4b1ffae7c750aaa3ee332437866879545
nicoperradin <nicoperradin@gmail.com>
2021-02-06 01:02:24 +0100

    Creation file database.php
    * Add connexion to the database
    
    * Add request update
    
    *Add request insert

---
+++ database.php
@@ -0,0 +1,50 @@
+class Database
+{
+
+	private $pdo;
+	private $table;
+
+	public function __construct(){
+		try{
+			$this->pdo = new \PDO(DBDRIVER.":dbname=".DBNAME.";host=".DBHOST.";port=".DBPORT,DBUSER,DBPWD);
+
+			if(ENV == "dev"){
+				$this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+	    		$this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+    		}
+
+		}catch(\Exception $e){
+			die("Erreur SQL " . $e->getMessage());
+		}
+
+		$getCalledClassExploded = explode("\\", get_called_class()); //App\Models\User
+		$this->table = DBPREFIXE.end($getCalledClassExploded);
+	}
+
+	public function save(){
+
+		$columns = array_diff_key (
+						get_object_vars($this),
+						get_class_vars(get_class())
+					);
+
+
+		if( is_null($this->getId()) ){
+			//INSERT
+			$query = $this->pdo->prepare("INSERT INTO ".$this->table." (".
+					implode(",", array_keys($columns))
+				.") 
+				VALUES ( :".
+					implode(",:", array_keys($columns))
+				." );");	
+		}else{
+			//UPDATE
+
+		}
+
+		$query->execute($columns);
+		
+
+
+	}
+}
